43fd98dd2f69e951ee98d546eac52c40d0e824c9
Yiwei <zyw870315@163.com>
2019-02-25 03:46:38 -0500

    del zip

---
+++ cmake-build-debug/CMakeFiles/cmake-build-debug/CMakeFiles/parking.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/parking.dir/Users/a41181/Desktop/Yiwei_Study_CS/COMP2401/A3/parkingSimulator.c.o"
+  "parking.pdb"
+  "parking"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/parking.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/cmake-build-debug/CMakeFiles/parking2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/parking2.dir/Users/a41181/Desktop/Yiwei_Study_CS/COMP2401/A3/simulator2.c.o"
+  "parking2.pdb"
+  "parking2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/parking2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
